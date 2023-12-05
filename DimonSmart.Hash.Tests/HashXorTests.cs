b54c6c314c4d3959a833f5ec57a03f86dabed4b0
Dmitry Dorogoy <dorogoj@live.ru>
2023-12-05 18:34:18 +0100

    Test logiing added

--- DimonSmart.Hash.Tests/HashXorTests.cs
+++ DimonSmart.Hash.Tests/HashXorTests.cs
@@ -1,15 +1,24 @@
-﻿using Xunit;
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace DimonSmart.Hash.Tests;
 
 public class XorHashTests
 {
+    private readonly ITestOutputHelper _output;
+
+    public XorHashTests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
     [Theory]
     [InlineData(0, 0)]
     [InlineData(1, 1)]
     [InlineData(0x80, 0x80)]
     [InlineData(0xFF, 0xFF)]
-    public void HashXorTests(byte expectedValue, byte addedValue)
+    public void HashOneByteTests(byte expectedValue, byte addedValue)
     {
         // Arrange
         var h = new XorHash(1);
@@ -26,7 +35,7 @@ public class XorHashTests
     [InlineData(0xFF, 0x00, 0xFF)]
     [InlineData(0xFF, 0xFF, 0x00)]
     [InlineData(0x80, 0x80, 0x00)]
-    public void XorHashShiftTest(byte expectedValue, byte initial, byte addedValue)
+    public void HashWithShiftTest(byte expectedValue, byte initial, byte addedValue)
     {
         // Arrange
         var h = new XorHash(1);
@@ -47,7 +56,8 @@ public class XorHashTests
         for (var i = 0; i < 8; i++)
         {
             h.AddByte(1);
-            var x = h.GetBytes().First();
+            var b = h.GetBytes().First();
+            _output.WriteLine($"HashValue: {b:X}");
         }
 
         Assert.Equal(new byte[] { 0xFF }, h.GetBytes());
